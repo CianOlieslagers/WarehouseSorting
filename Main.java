d305bda3413a4d3fec30eeff310d705dabc71cff
PhillipP662 <phillip.p.p13@gmail.com>
2024-10-09 10:58:34 +0200

    Duidelijkere map

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         // Create a map using the string representation
-        String mapString =  "1 1 1 1 1 1 1 1 1 1 1 1 1\n" +
-                            "1 0 $ 0 $ 0 $ 0 $ 0 $ 0 1\n" +
-                            "1 0 0 0 0 0 0 0 0 0 0 $ 1\n" +
-                            "1 0 0 0 0 0 0 0 0 0 0 0 1\n" +
-                            "1 0 0 0 0 0 0 0 0 0 0 $ 1\n" +
-                            "_ 0 0 0 0 0 0 0 0 0 0 0 1\n" +
-                            "1 0 0 0 0 0 0 0 0 0 0 $ 1\n" +
-                            "1 0 & 0 0 0 $ 0 $ 0 $ 0 1\n" +
-                            "1 1 1 1 1 1 1 1 1 1 1 1 1";
+        String mapString =  "# # # # # # # # # # # # #\n" +
+                            "# . $ . $ . $ . $ . $ . #\n" +
+                            "# . . . . . . . . . . $ #\n" +
+                            "# . . . . . . . . . . . #\n" +
+                            "# . . . . . . . . . . $ #\n" +
+                            "_ . . . . . . . . . . . #\n" +
+                            "# . . . . . . . . . . $ #\n" +
+                            "# . & . . . $ . $ . $ . #\n" +
+                            "# # # # # # # # # # # # #";
 
         Map myMap = new Map(mapString);
 
